c21c44ab7687f65c441b0283640ad78e748e6a86
Tyler Gasperlin <gasperlint@gmail.com>
2020-11-26 14:05:33 -0800

    Ingress config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,11 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Hi there!!')
+});
+
 app.listen(3000, () => {
     console.log('listening on 3000!!!!!!!!!')
 });
+
