1b91b11d6b1ea4738e27beb5dc89ca8260872056
Tyler Gasperlin <gasperlint@gmail.com>
2020-11-27 10:27:04 -0800

    Respond to error objects through the middleware

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,8 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
+import { RequestValidationError } from '../errors/reqeust-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
 
-    console.log('Something went wrong: ', err)
+    if (err instanceof RequestValidationError) {
+        console.log('handling error as request validation error')
+    }
+
+    if (err instanceof DatabaseConnectionError) {
+        console.log('handling error as database connection error')
+    }
 
     res.status(400).send({ 
         message: err.message
