7a17d1367716cbced23091d6f5c16b2ad53784ff
Tyler Gasperlin <gasperlint@gmail.com>
2020-11-27 10:09:16 -0800

    Use err messages instead of hard code string

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -5,7 +5,7 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
     console.log('Something went wrong: ', err)
 
     res.status(400).send({ 
-        message: 'Something went wrong'
+        message: err.message
     })
 
 }
