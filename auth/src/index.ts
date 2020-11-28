8d1d5f4cbef383f55f35d701ef2f75ee8cf0241a
Tyler Gasperlin <gasperlint@gmail.com>
2020-11-27 19:10:27 -0800

    Add connection to mongodb in the client

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/signin';
 import { signOutRouter } from './routes/signout';
@@ -22,7 +23,23 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log('listening on 3000!!!!!!!!!!')
-});
+(async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        console.log('Connected to mongoDb')
+    } catch (err) {
+        console.error(err)
+    }
+
+    app.listen(3000, () => {
+        console.log('listening on 3000!!!!!!!!!!')
+    });
+    
+
+})()
+
 
