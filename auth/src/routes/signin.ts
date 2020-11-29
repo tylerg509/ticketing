ad804aca324334638db6b2a029f3062f5b6b2759
Tyler Gasperlin <gasperlint@gmail.com>
2020-11-29 09:20:59 -0800

    Start to build out signin validations

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator'
+import { RequestValidationError } from '../errors/reqeust-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
+router.post('/api/users/signin', 
+[ 
+    body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+
+    body('password')
+        .trim()
+        .notEmpty()
+        .withMessage('You must supply a password')
+],
+(req: Request, res: Response) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        throw new RequestValidationError(errors.array())
+    }
     res.send('Hi there!!')
 });
 
