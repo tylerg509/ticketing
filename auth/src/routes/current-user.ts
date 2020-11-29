7c3fd01aec2bdd6736c8f59346ba08b2ee60e22a
Tyler Gasperlin <gasperlint@gmail.com>
2020-11-29 11:50:07 -0800

    Build middleware to handle unauthorized

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,5 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import { EnvVariables } from '../helpers/constants';
+
 import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
