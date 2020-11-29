1f36cc486537cc20abb1314fdcdeff09e7a99d43
Tyler Gasperlin <gasperlint@gmail.com>
2020-11-29 11:12:30 -0800

    Build signout

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,10 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-    res.send('Hi there!!')
+    // per cookie session set session to null
+    req.session = null;
+
+    res.send({})
 });
 
 export { router as signOutRouter }
