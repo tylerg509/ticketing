afde074ecad99805c1eed5e88eb86d227d37a3cc
Tyler Gasperlin <gasperlint@gmail.com>
2020-11-29 11:00:38 -0800

    Ability to get current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,25 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!!')
+    if (!req.session?.jwt) {
+        return res.send({ currentUser: null });
+    }
+
+    // verify jwt token
+    // BANG is ok since we verify existence of jwt_key in index.js
+    try {
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!); 
+
+        res.send({ currentUser: payload})
+        
+    } catch(err) {
+        res.send({currentUser: null})
+    }
 });
 
-export { router as currentUserRouter }
+export { router as currentUserRouter };
+
+
