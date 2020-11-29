86ab2c132df20cd6122161b01f2e48482f991a45
Tyler Gasperlin <gasperlint@gmail.com>
2020-11-29 12:51:36 -0800

    Refactor index into app comp

---
+++ auth/src/app.ts
@@ -0,0 +1,37 @@
+import 'express-async-errors';
+
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import express from 'express';
+
+import { NotFoundError } from './errors/not-found-error';
+import { errorHandler } from './middlewares/error-handler';
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+import { signUpRouter } from './routes/signup';
+
+const app = express();
+
+// To ensure that express knows that are using ngnix and that express is behind the ngnix proxy and this proxy is secure
+app.set('trust proxy', true)
+app.use(json());
+
+// We need to use a cookie because nextjs app will be rendered server side
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}))
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError()
+})
+
+app.use(errorHandler);
+
+export { app }
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,39 +1,10 @@
 import 'express-async-errors';
 
-import { json } from 'body-parser';
-import express from 'express';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-import { NotFoundError } from './errors/not-found-error';
-import { errorHandler } from './middlewares/error-handler';
-import { currentUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/signin';
-import { signOutRouter } from './routes/signout';
-import { signUpRouter } from './routes/signup';
+import { app } from './app';
+
 import { EnvVariables } from './helpers/constants';
 
-const app = express();
-
-// To ensure that express knows that are using ngnix and that express is behind the ngnix proxy and this proxy is secure
-app.set('trust proxy', true)
-app.use(json());
-
-// We need to use a cookie because nextjs app will be rendered server side
-app.use(cookieSession({
-    signed: false,
-    secure: true
-}))
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-app.all('*', async (req, res) => {
-    throw new NotFoundError()
-})
-
-app.use(errorHandler);
 
 // immediately run this startup function
 (async () => {
