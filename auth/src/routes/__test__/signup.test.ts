ef54108f9806082abf44a3d320f42e731589ec86
Tyler Gasperlin <gasperlint@gmail.com>
2020-11-29 13:45:56 -0800

    Add tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,39 @@ it('returns a 201 on successful signup', async() => {
         })
         .expect(201)
 });
+
+it('returns 400 with an invalid email', async () => {
+    return request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'testdsads',
+        password: 'password'
+    })
+    .expect(400)
+})
+
+it('returns 400 with an invalid password', async () => {
+    return request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'test@test.com',
+        password: ''
+    })
+    .expect(400)
+})
+
+it('returns 400 when missing email and password', async () => {
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'test@test.com'
+    })
+    .expect(400)
+
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        password: 'tasdasfasfasfa'
+    })
+    .expect(400)
+})
