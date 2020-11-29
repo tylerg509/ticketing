fc096c63cafd24a26267d0476a7e50080f8f0603
Tyler Gasperlin <gasperlint@gmail.com>
2020-11-29 13:04:23 -0800

    Create a setup file for testing

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,28 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose'
+import { app } from '../app';
+
+let mongo: any;
+
+beforeAll(async () => {
+    mongo = new MongoMemoryServer();
+    const mongoUri = await mongo.getUri();
+
+    await mongoose.connect(mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+})
+
+beforeEach( async () => {
+    const collections = await mongoose.connection.db.collections();
+
+    collections.forEach(async (element) => {
+        await element.deleteMany({})
+    });
+})
+
+afterAll(async () => {
+    await mongo.stop();
+    await mongoose.connection.close();
+})
