ef15cb81fd628a273521ac9990ea85ee7529d0d2
Tyler Gasperlin <gasperlint@gmail.com>
2020-11-29 14:20:10 -0800

    Add helper for handling cookies in tests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -2,12 +2,22 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose'
 import { app } from '../app';
 import { EnvVariables } from '../helpers/constants';
+import request from 'supertest'
+
+
+declare global {
+    namespace NodeJS {
+        interface Global {
+            signin(): Promise<string[]>
+        }
+    }
+}
 
 let mongo: any;
 
 beforeAll(async () => {
     process.env[EnvVariables.JWT_KEY] = 'asdfasdf'
-    
+
     mongo = new MongoMemoryServer();
     const mongoUri = await mongo.getUri();
 
@@ -29,3 +39,20 @@ afterAll(async () => {
     await mongo.stop();
     await mongoose.connection.close();
 })
+
+global.signin = async () => {
+    const email = 'test@test.com';
+    const password = 'password'
+
+    const authResponse = await request(app)
+    .post('/api/users/signup')
+    .send({
+        email: email,
+        password: password,
+    })
+    .expect(201);  
+
+    const cookie = authResponse.get('Set-Cookie')
+
+    return cookie
+}
