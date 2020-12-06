b95e8f7e35200a5a435be3263b4721883f7e6bc7
Tyler Gasperlin <gasperlint@gmail.com>
2020-12-06 08:42:30 -0800

    Update user model for bugs

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -26,31 +26,36 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
-}, {toJSON: {                  // toJson is allowing us to customize the object that is returned from mongodb // this is not a good approach - its like making a sql view or something
-    transform(doc, ret) {
-        ret.id = ret._id;   // we change to id in order to keep this consistent across other databases (if applicable in future). _id is mongodb specific implementation
-        delete ret._id
-        delete ret.password // remove password from the return object
-        delete ret.__v // remove mongodb prop
-    }
-} } )
+})
 
+userSchema.set("toJSON", {
+    transform: (doc: any, returned: any) => {
+      returned.id = returned._id;
+      delete returned._id;
+      delete returned.password; // remove password field in returned object
+      delete returned.__v;
+    },
+  });
+  
 // if you use an arrow function 'this' would reference the entire docuemnt instead of the function
 // pre save = a middleware function within mongoose. Every time you try to save something run this function
 // mongoose does not support async await very well so we use the done callback to handle async code
-userSchema.pre('save', async function(done) {
-    if(this.isModified('password')) {
-        const hashed = await Password.toHash(this.get('password'));
-        this.set('password', hashed);
-    }
 
-    done()
-})
+//* Pre save hook run everytime we save runs every time
+userSchema.pre("save", async function () {
+    // we have to pass in done as mongose does not know when the async function endds
+    if (this.isModified("password")) {
+      const hashed = await Password.toHash(
+        this.get("password")
+      );
+      this.set("password", hashed);
+    }
+  });
 
 // add a static method to the user schema for type checking
-userSchema.statics.build = (attrs: IUserAttrs) => {
-    return new User(attrs)
-}
+userSchema.static("build", (attrs: IUserAttrs) => {
+    return new User(attrs);
+  });
 
 // IUserModels adds typing to the static methods
 const User = mongoose.model<IUserDoc, IUserModel>('User', userSchema);
