74450ade3d9128faacd365e03767067280ad9132
Tyler Gasperlin <gasperlint@gmail.com>
2021-02-06 15:30:38 -0800

    Setup ticking tests

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('has a route handler listening to /api/tickets for post requests', async () => {
+    
+})
+
+it('can only be acessed if the user is signed in', async () => {
+
+})
+
+it('returns an error if an invalid title is provided', async () => {
+
+})
+
+it('creates a tiket with valid parameters', async () => {
+
+})
