8417b28912a010754ed013a6cce78c33e0d805d6
Tyler Gasperlin <gasperlint@gmail.com>
2021-02-06 18:52:27 -0800

    Build out tests for ticketing data validation post

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -16,11 +16,47 @@ it('returns a status othr than 401 if the user is signed in', async () => {
     const response = await request(app)
         .post('/api/tickets')
         .set('Cookie', global.signin())
-        .send({})
+        .send({});
 
     expect(response.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid title is provided', async () => {});
+it('returns an error if an invalid title is provided', async () => {
+    const result = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: '',
+            price: 10,
+        })
+        .expect(400)
 
-it('creates a tiket with valid parameters', async () => {});
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            price: 10,
+        })
+        .expect(400);
+});
+
+it('returns an error if an invalid price is provided', async () => {
+    const result = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'gooa3efa',
+            price: -10,
+        })
+        .expect(400);
+
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'gooa3efa'
+        })
+        .expect(400);
+});
+
+it('creates a ticket with valid inputs', async () => {});
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,10 @@
 import { requireAuth } from '@tylergasperlin/ticketing-common'
-import express, { Request, response, Response} from 'express'
+import express, { Request, Response} from 'express'
 
 const router = express.Router()
 
 router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-    response.sendStatus(200)
+    res.sendStatus(200)
 });
 
 
