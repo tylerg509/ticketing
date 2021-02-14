748cc6ab8e404bf4e6bcdc746b347c3617e25ee5
Tyler Gasperlin <gasperlint@gmail.com>
2021-02-13 16:25:59 -0800

    Finish building out update ticket

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,11 +1,20 @@
-import { NotAuthorizedError, NotFoundError, requireAuth } from '@tylergasperlin/ticketing-common';
+import { NotAuthorizedError, NotFoundError, requireAuth, validateRequest } from '@tylergasperlin/ticketing-common';
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.put('/api/tickets/:id', requireAuth ,async (req: Request, res: Response) => {
+router.put('/api/tickets/:id', requireAuth, [
+    body('title')
+        .not()
+        .isEmpty()
+        .withMessage('title is required'),
+    body('price')
+        .isFloat({gt: 0})
+        .withMessage('price must be provided and must be greater than 0')
+], validateRequest,
+ async (req: Request, res: Response) => {
     const ticket = await Ticket.findById(req.params.id)
 
     if(!ticket) {
@@ -15,6 +24,13 @@ router.put('/api/tickets/:id', requireAuth ,async (req: Request, res: Response) 
     if(ticket.userId !== req.currentUser!.id) {
         throw new NotAuthorizedError();
     }
+
+    ticket.set({
+        title: req.body.title,
+        price: req.body.price
+    })
+
+    await ticket.save()
 
     res.send(ticket)
 })
