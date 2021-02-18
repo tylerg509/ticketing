b8acdd018d0d17433055a2cf0c9745e3787e6532
Tyler Gasperlin <gasperlint@gmail.com>
2021-02-17 19:53:04 -0800

    Add publisher event publishing

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,9 +1,22 @@
 import nats from 'node-nats-streaming'
 
+/**
+ * See readme - need to enable port forwarding or another option for connection to 4222
+ */
 const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222'
 });
 
 stan.on('connect', () => {
     console.log('Publisher connected to NATS')
+
+    const data = JSON.stringify({
+        id: '123',
+        title: 'concert',
+        price: 20
+    })
+
+    stan.publish('ticket:created', data, () => {
+        console.log('event published')
+    })
 })
