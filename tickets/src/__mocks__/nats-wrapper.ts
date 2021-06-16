c4cb48b5f32e533d8eba13ea289dfca589f1bc03
Tyler Gasperlin <gasperlint@gmail.com>
2021-06-15 17:02:20 -0700

    Use the mock implemenation of natswrapper

--- tickets/src/__mocks__/nats-wrapper.ts
+++ tickets/src/__mocks__/nats-wrapper.ts
@@ -1,1 +1,10 @@
-export const natsWrapper = { }
+/**
+ * this is a mock of natswapper within base-publisher.ts. For use in automated tests
+ */
+export const natsWrapper = { 
+    client:  { 
+        publish: (subject: string, data: string, callback: () => void) => {
+            callback();
+        }   
+    }
+}
