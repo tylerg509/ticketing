fef186ea87a7e4640fa5a6eec5aa78bf9813bb59
Tyler Gasperlin <gasperlint@gmail.com>
2021-06-19 12:57:08 -0700

    Start build out post new order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,8 @@
-import { requireAuth, validateRequest } from '@tylergasperlin/ticketing-common';
+import { NotFoundError, requireAuth, validateRequest } from '@tylergasperlin/ticketing-common';
 import express, { Response, Request } from 'express';
 import { body } from 'express-validator';
 import mongoose from 'mongoose'
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
@@ -12,6 +13,24 @@ router.post('api/orders', requireAuth, [
     .custom((input: string) => mongoose.Types.ObjectId.isValid(input)) // creates somewhat tight coupling with mongoose. If we change database this would need to be removed
     .withMessage('TicketId must be provided')
 ], validateRequest, async (req: Request, res: Response) => {
+    // find ticket in db that user is trying to purchase
+    const { ticketId } = req.body
+
+    const ticket = await Ticket.findById(ticketId)
+
+
+    if (!ticket) {
+        throw new NotFoundError();
+    }
+    // make sure ticket is not reserved 
+
+    // calc expiration of reserved ticket
+
+    // build order and save to db
+
+    // publish event that order was created 
+
+
     res.send({})
 })
 
