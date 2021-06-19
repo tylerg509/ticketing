abc0c312a5abdd586ea1705adb73f503e90a3e00
Tyler Gasperlin <gasperlint@gmail.com>
2021-06-19 08:52:45 -0700

    Order new handling

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,17 @@
+import { requireAuth, validateRequest } from '@tylergasperlin/ticketing-common';
 import express, { Response, Request } from 'express';
-
+import { body } from 'express-validator';
+import mongoose from 'mongoose'
 
 const router = express.Router();
 
-router.post('api/orders', async (req: Request, res: Response) => {
+router.post('api/orders', requireAuth, [
+    body('ticketId')
+    .not()
+    .isEmpty()
+    .custom((input: string) => mongoose.Types.ObjectId.isValid(input)) // creates somewhat tight coupling with mongoose. If we change database this would need to be removed
+    .withMessage('TicketId must be provided')
+], validateRequest, async (req: Request, res: Response) => {
     res.send({})
 })
 
