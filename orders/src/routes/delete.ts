cfc69defb469a8fdf4ec085e3432c62f6e89f59e
Tyler Gasperlin <gasperlint@gmail.com>
2021-06-19 21:24:45 -0700

    delete order

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,10 +1,25 @@
+import { NotAuthorizedError, NotFoundError, OrderStatus, requireAuth } from '@tylergasperlin/ticketing-common';
 import express, { Response, Request } from 'express';
+import { Order } from '../models/order';
 
 
 const router = express.Router();
 
-router.delete('api/orders/:orderId', async (req: Request, res: Response) => {
-    res.send({})
+router.delete('api/orders/:orderId', requireAuth,  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate('ticket') // should verify orderid first
+
+    if(!order) {
+        throw new NotFoundError()
+    }
+
+    if(order.userId !== req.currentUser!.id) {
+        throw new NotAuthorizedError()
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+    
+    res.send(order)
 })
 
 export { router as deleteOrderRouter }
