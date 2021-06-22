0dbf65659fa7cbbc9fafc34da66c4490906719d8
Tyler Gasperlin <gasperlint@gmail.com>
2021-06-21 19:57:57 -0700

    Ticket updated listener

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,27 @@
+import { Listener, Subjects, TicketUpdatedEvent } from '@tylergasperlin/ticketing-common';
+import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming'
+import { Ticket } from '../../models/ticket';
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+    subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+        const ticket = await Ticket.findById(data.id)
+
+        if(!ticket) {
+            throw new Error('ticket not found')
+        }
+
+        const { title, price } = data;
+        ticket.set({
+            title, price
+        })
+
+        await ticket.save()
+
+        msg.ack()
+    }
+}
