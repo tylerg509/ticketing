4e7820cf1845632e7ab387ebdd306f10f44904bf
Tyler Gasperlin <gasperlint@gmail.com>
2021-06-21 18:51:56 -0700

    Implement new order events

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -6,6 +6,7 @@ import {
 } from '@tylergasperlin/ticketing-common';
 import { Order, OrderStatus } from '../models/order';
 import { natsWrapper } from '../nats-wrapper';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
 
 const router = express.Router();
 
@@ -27,7 +28,12 @@ router.delete(
     await order.save();
 
     // publishing an event saying this was cancelled!
-
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+        id: order.id,
+        ticket: {
+            id: order.ticket.id
+        }
+    })
 
     res.status(204).send(order);
   }
